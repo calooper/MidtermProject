6c016ad6484238745a705043412cbbd2fbc6938a
Jacob <jacobshorterivey@gmail.com>
2019-11-14 14:16:46 -0700

    stub Boot/MVC project, link JPA project

---
+++ NomadicGardens/src/main/java/com/skilldistillery/nomadicgardens/NomadicGardensApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.nomadicgardens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NomadicGardensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NomadicGardensApplication.class, args);
+	}
+
+}
---
+++ NomadicGardens/src/test/java/com/skilldistillery/nomadicgardens/NomadicGardensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.nomadicgardens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NomadicGardensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
