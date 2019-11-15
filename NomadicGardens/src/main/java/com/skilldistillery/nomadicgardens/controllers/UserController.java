99803a6240ab2ba2070a3f23f997271117f6da11
Briman-Jag <brianstreetman25@gmail.com>
2019-11-15 16:05:54 -0700

    Added code to UserController

--- NomadicGardens/src/main/java/com/skilldistillery/nomadicgardens/controllers/UserController.java
+++ NomadicGardens/src/main/java/com/skilldistillery/nomadicgardens/controllers/UserController.java
@@ -1,5 +1,22 @@
 package com.skilldistillery.nomadicgardens.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.skilldistillery.nomadicgardens.dao.UserDAO;
+
+@Controller
 public class UserController {
+	
+	@Autowired
+	private UserDAO userDAO;
+	
+	@RequestMapping(path="/", method = RequestMethod.GET)
+	public String index() {
+	  
+	  return "home";
+	}
 
 }
